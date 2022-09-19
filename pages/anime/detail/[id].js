b82e419949be096a5677ad36731c227189801f07
masagis <wahyujiat@gmail.com>
2022-09-20 00:01:03 +0700

    feat: detail anime unfinish

---
+++ pages/anime/detail/[id].js
@@ -0,0 +1,10 @@
+import React from 'react'
+import DetailByID from '@containers/detail/Index'
+
+export default function Index() {
+  return (
+    <div>
+      <DetailByID />
+    </div>
+  )
+}
---
+++ src/containers/detail/Index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Index() {
+  return (
+    <div className="bg-white container">
+      <h1>Detail</h1>
+    </div>
+  )
+}
+
+export default Index
